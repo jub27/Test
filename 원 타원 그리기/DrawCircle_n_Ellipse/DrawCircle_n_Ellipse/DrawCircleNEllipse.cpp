b09d5f585f79845f2fe227a8758f1a1a1c364eb0
jub27 <dosss2726@gmail.com>
2019-12-20 14:19:11 +0900

    원 타원 그리기 제출

--- 원 타원 그리기/DrawCircle_n_Ellipse/DrawCircle_n_Ellipse/DrawCircleNEllipse.cpp
+++ 원 타원 그리기/DrawCircle_n_Ellipse/DrawCircle_n_Ellipse/DrawCircleNEllipse.cpp
@@ -2,8 +2,8 @@
 #include <math.h>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-HINSTANCE g_hInst;//글로벌 인스턴스핸들값
-LPCTSTR lpszClass = TEXT("HelloWorld"); //클래스 명 : 창이름
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("HelloWorld");
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmdParam, int nCmdShow)
 {
@@ -11,25 +11,25 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmd
 	MSG Message;
 	WNDCLASS WndClass;
 	g_hInst = hInstance;
-	//WndClass는 기본 윈도우환경을 만드는 구조체다. 맴버변수는 밑에와 같다.
-	WndClass.cbClsExtra = 0; //예약영역
-	WndClass.cbWndExtra = 0; //예약영역 (신경x)
-	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //배경색
-	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW); //커서
-	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION); //아이콘 모양
-	WndClass.hInstance = hInstance; //(프로그램 핸들값(번호)등록)
-	WndClass.lpfnWndProc = WndProc; //프로세스 함수 호출
-	WndClass.lpszClassName = lpszClass; //클레스 이름
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
 	WndClass.lpszMenuName = NULL;
-	WndClass.style = CS_HREDRAW | CS_VREDRAW; //윈도우의 수직과 수평이 변경 시 다시 그린다.
-	RegisterClass(&WndClass); //만들어진 WidClass를 등록
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
 	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL,
 		(HMENU)NULL, hInstance, NULL);
 	ShowWindow(hWnd, nCmdShow);
-	while (GetMessage(&Message, NULL, 0, 0)) //사용자에게 메시지를 받아오는 함수(WM_QUIT 메시지 받을 시 종료), 메시지가 오기까지 대기를 하고있다가 오면 작동.
+	while (GetMessage(&Message, NULL, 0, 0))
 	{
-		TranslateMessage(&Message); // 키보드 입력 메시지 처리함수
-		DispatchMessage(&Message); //받은 메시지를 WndProc에 전달하는 함수
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
 	}
 	return (int)Message.wParam;
 }
@@ -59,6 +59,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 			SetPixel(hdc, x, center_y - y, RGB(0, 0, 0));
 			SetPixel(hdc, x, center_y + y, RGB(0, 0, 0));
 		}
+		EndPaint(hWnd, &ps);
 		return 0;
 	}
 	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
