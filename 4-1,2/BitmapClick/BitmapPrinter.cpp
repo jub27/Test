46f319e5f0706706c9d6ef4f68482d8fd3741156
jub27 <dosss2726@gmail.com>
2019-12-26 14:46:04 +0900

    4-1,2 제출

--- 4-1,2/BitmapClick/BitmapPrinter.cpp
+++ 4-1,2/BitmapClick/BitmapPrinter.cpp
@@ -16,7 +16,7 @@ void BitmapPrinter::SetInstance(HINSTANCE hInstance) {
 }
 
 void BitmapPrinter::Print(int resource) {
-	myBitmap = LoadBitmap(hInstance, MAKEINTRESOURCE(IDB_BITMAP1));
+	myBitmap = LoadBitmap(hInstance, MAKEINTRESOURCE(resource));
 	oldBitmap = (HBITMAP)SelectObject(MemDC, myBitmap);
 	BitBlt(hdc, BITMAP_X, BITMAP_Y, BITMAP_WIDTH, BITMAP_HEIGHT, MemDC, 0, 0, SRCCOPY);
 	SelectObject(MemDC, oldBitmap);
