7fdecc7fbe47be2321b92db80b5bd83ded2071f0
LEE-DO-HUN <tkdgus4977@naver.com>
2020-06-26 13:04:07 +0900

    카메라 제출

---
+++ 제출/3D_5_2_Camera/Camera2/Camera.h
@@ -0,0 +1,49 @@
+#pragma once
+#ifndef _ZCAMERA_H_
+#define _ZCAMERA_H_
+
+#include <d3d9.h>
+#include <d3dx9.h>
+
+class Camera
+{
+private:
+	D3DXVECTOR3 m_vEye;
+	D3DXVECTOR3 m_vLookAt;
+	D3DXVECTOR3 m_vUp;
+
+	D3DXVECTOR3 m_vView;
+	D3DXVECTOR3 m_vCross;
+
+	D3DXMATRIXA16 m_matView;
+	D3DXMATRIXA16 m_matBill;
+	D3DXMATRIXA16* m_matBlock;
+
+	float rotateX, rotateY, rotateZ;
+public:
+	Camera();
+	float GetRotateX() { return rotateX; }
+	float GetRotateY() { return rotateY; }
+	float GetRotateZ() { return rotateZ; }
+	D3DXMATRIXA16* GetViewMatrix() { return &m_matView; }
+	D3DXMATRIXA16* GetBillMatrix() { return &m_matBill; }
+	D3DXMATRIXA16* SetView(D3DXVECTOR3* pvEye, D3DXVECTOR3* pvLookAt, D3DXVECTOR3* pvUp);
+	void SetEye(D3DXVECTOR3* pv) { m_vEye = *pv; }
+	D3DXVECTOR3* GetCross() { return &m_vCross; }
+	D3DXVECTOR3* GetEye() { return &m_vEye; }
+	void SetLookAt(D3DXVECTOR3* pv) { m_vLookAt = *pv; }
+	D3DXVECTOR3* GetLookat() { return &m_vLookAt; }
+	void SetUp(D3DXVECTOR3* pv) { m_vUp = *pv; }
+	D3DXVECTOR3* GetUp() { return &m_vUp; }
+	D3DXVECTOR3* GetView() { return &m_vView; }
+	void Flush() { SetView(&m_vEye, &m_vLookAt, &m_vUp); }
+	D3DXMATRIXA16* RotateLocalX(float angle);
+	D3DXMATRIXA16* RotateLocalY(float angle);
+	D3DXMATRIXA16* RotateLocalZ(float angle);
+	D3DXMATRIXA16* MoveTo(D3DXVECTOR3* pv);
+	D3DXMATRIXA16* MoveLocalX(float dist);
+	D3DXMATRIXA16* MoveLocalY(float dist);
+	D3DXMATRIXA16* MoveLocalZ(float dist);
+};
+
+#endif
