1703acbe613f7f0253173543dd8d182131015d32
jub27 <55947009+jub27@users.noreply.github.com>
2020-07-15 11:49:41 +0900

    unity_1_제출

---
+++ 제출/Unity_1_prefab/Assets/Scripts/MakeBullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBullet : MonoBehaviour
+{
+
+    public float dealay = 2;
+    private float curTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curTime += Time.deltaTime;
+        if (curTime >= dealay)
+        {
+            GameObject obj = Instantiate(Resources.Load("Bullet"), transform) as GameObject;
+            curTime = 0;
+        }
+    }
+}
