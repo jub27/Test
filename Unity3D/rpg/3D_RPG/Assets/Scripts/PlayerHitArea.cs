e3e7e400ce4b58ab5f67d166f714131219a5d6f5
jub27 <55947009+jub27@users.noreply.github.com>
2020-08-14 11:46:48 +0900

    rpg

---
+++ Unity3D/rpg/3D_RPG/Assets/Scripts/PlayerHitArea.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitArea : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = transform.root.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.root.gameObject.GetComponent<CharacterMove>().OnDamage();
+    }
+}
